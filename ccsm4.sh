f9e041e6c85284a1c469f29b87d14f4c89d31ca9
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-28 18:47:45 +0530

    * ccsm4.sh: working cleaned uup.

--- ccsm4.sh
+++ ccsm4.sh
@@ -1,79 +1,50 @@
 #!/bin/bash
 # Created: Thursday, July 13 2017
 
-exp=${1:-historical}
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_FILE_DIR/fun.bash
+
+# exp=${1:-historical}
+# exmts=(historical)
+exmts=(historical historicalNat historicalMisc historicalGHG)
+
+vars=(pr)
+# vars=(pr tas tasmax tasmin)
+
 model=CCSM4
 ens=r1i1p1
 
-case ${exp} in
-    historical )
-        dir=hist
-        ;;
-    historicalNat )
-        dir=histNat
-        ;;
-    historicalMisc )
-        dir=histMisc
-        ens=r1i1p10
-        ;;
-    historicalGHG )
-        dir=histGHG
-        ;;
-esac
+for exp in ${exmts[@]}; do
+    case ${exp} in
+        historical )
+            dir=hist
+            ;;
+        historicalNat )
+            dir=histNat
+            ;;
+        historicalGHG )
+            dir=histGHG
+            ;;
+        historicalMisc )
+            dir=histMisc
+            ens=r1i1p10
+            ;;
+    esac
 
-cd ${dir}
+    cd $THIS_FILE_DIR/${dir}
 
-echo "Started processing ${exp} in ${dir} directory."
+    for var in "${vars[@]}"
+    do
+        cdo -mergetime \
+            ${var}_day_${model}_${exp}_${ens}_18850101-19191231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19200101-19541231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19550101-19891231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19900101-20051231.nc \
+            ${var}_day_${model}_${exp}_${ens}_18850101-20051231.nc
 
-if ls -U *${model}*${exp}*r180x100* 1> /dev/null 2>&1; then
-    echo Files seems already processed.
-    exit
-fi
+        cdo -remapbil,r180x100 -selyear,1900/2005 ${var}_day_${model}_${exp}_${ens}_18850101-20051231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
 
-cdo -mergetime \
-    tas_day_${model}_${exp}_${ens}_18850101-19191231.nc \
-    tas_day_${model}_${exp}_${ens}_19200101-19541231.nc \
-    tas_day_${model}_${exp}_${ens}_19550101-19891231.nc \
-    tas_day_${model}_${exp}_${ens}_19900101-20051231.nc \
-    tas_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -remapbil,r180x100 -selyear,1900/2005 tas_day_${model}_${exp}_${ens}_18850101-20051231.nc \
-    tas_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tas_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -mergetime \
-    tasmin_day_${model}_${exp}_${ens}_18850101-19191231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19200101-19541231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19550101-19891231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19900101-20051231.nc \
-    tasmin_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -remapbil,r180x100 -selyear,1900/2005 tasmin_day_${model}_${exp}_${ens}_18850101-20051231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tasmin_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -mergetime \
-    tasmax_day_${model}_${exp}_${ens}_18850101-19191231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19200101-19541231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19550101-19891231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19900101-20051231.nc \
-    tasmax_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -remapbil,r180x100 -selyear,1900/2005 tasmax_day_${model}_${exp}_${ens}_18850101-20051231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tasmax_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -mergetime \
-    pr_day_${model}_${exp}_${ens}_18850101-19191231.nc \
-    pr_day_${model}_${exp}_${ens}_19200101-19541231.nc \
-    pr_day_${model}_${exp}_${ens}_19550101-19891231.nc \
-    pr_day_${model}_${exp}_${ens}_19900101-20051231.nc \
-    pr_day_${model}_${exp}_${ens}_18850101-20051231.nc
-
-cdo -remapbil,r180x100 -selyear,1900/2005 pr_day_${model}_${exp}_${ens}_18850101-20051231.nc \
-    pr_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm pr_day_${model}_${exp}_${ens}_18850101-20051231.nc
+        rm ${var}_day_${model}_${exp}_${ens}_18850101-20051231.nc
+    done
+done
