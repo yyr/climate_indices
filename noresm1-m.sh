eef6f4e77a4799b5e0df065aabc402ebeaef5111
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-28 19:31:08 +0530

    * noresm1-m.sh: working.

--- noresm1-m.sh
+++ noresm1-m.sh
@@ -1,65 +1,50 @@
 #!/bin/bash
 # Created: Wednesday, July 12 2017
-exp=${1:-historical}
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_FILE_DIR/fun.bash
+
+# exp=${1:-historical}
+# exmts=(historical)
+exmts=(historical historicalNat historicalGHG historicalMisc)
+
+vars=(pr)
+# vars=(pr tas tasmax tasmin)
+
 model=NorESM1-M
 ens=r1i1p1
 
-case ${exp} in
-    historical )
-        dir=hist
-        ;;
-    historicalNat )
-        dir=histNat
-        ;;
-    historicalMisc )
-        dir=histMisc
-        ;;
-    historicalGHG )
-        dir=histGHG
-        ;;
-esac
+for exp in ${exmts[@]}; do
+    case ${exp} in
+        historical )
+            dir=hist
+            ;;
+        historicalNat )
+            dir=histNat
+            ;;
+        historicalGHG )
+            dir=histGHG
+            ;;
+        historicalMisc )
+            dir=histMisc
+            ;;
+    esac
 
-cd ${dir}
+    cd $THIS_FILE_DIR/${dir}
 
-echo "Started processing ${exp} in ${dir} directory."
+    echo "Started processing ${exp} in ${dir} directory."
 
-if ls -U *${model}*${exp}*r180x100* 1> /dev/null 2>&1; then
-    echo Files seems already processed.
-    exit
-fi
+    for var in "${vars[@]}"
+    do
+        cdo -mergetime \
+            ${var}_day_${model}_${exp}_${ens}_18500101-18991231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19000101-19491231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19500101-19991231.nc \
+            ${var}_day_${model}_${exp}_${ens}_20000101-20051231.nc \
+            ${var}_day_${model}_${exp}_${ens}_18500101-20051231.nc
 
-cdo -mergetime \
-    tas_day_${model}_${exp}_${ens}_18500101-18991231.nc \
-    tas_day_${model}_${exp}_${ens}_19000101-19491231.nc \
-    tas_day_${model}_${exp}_${ens}_19500101-19991231.nc \
-    tas_day_${model}_${exp}_${ens}_20000101-20051231.nc \
-    tas_day_${model}_${exp}_${ens}_18500101-20051231.nc
+        cdo -remapbil,r180x100 ${var}_day_${model}_${exp}_${ens}_18500101-20051231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
 
-cdo -remapbil,r180x100 tas_day_${model}_${exp}_${ens}_18500101-20051231.nc \
-    tas_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tas_day_${model}_${exp}_${ens}_18500101-20051231.nc
-
-cdo -mergetime \
-    tasmin_day_${model}_${exp}_${ens}_18500101-18991231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19000101-19491231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19500101-19991231.nc \
-    tasmin_day_${model}_${exp}_${ens}_20000101-20051231.nc \
-    tasmin_day_${model}_${exp}_${ens}_18500101-20051231.nc
-
-cdo -remapbil,r180x100 tasmin_day_${model}_${exp}_${ens}_18500101-20051231.nc \
-    tasmin_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tasmin_day_${model}_${exp}_${ens}_18500101-20051231.nc
-
-cdo -mergetime \
-    tasmax_day_${model}_${exp}_${ens}_18500101-18991231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19000101-19491231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19500101-19991231.nc \
-    tasmax_day_${model}_${exp}_${ens}_20000101-20051231.nc \
-    tasmax_day_${model}_${exp}_${ens}_18500101-20051231.nc
-
-cdo -remapbil,r180x100 tasmax_day_${model}_${exp}_${ens}_18500101-20051231.nc \
-    tasmax_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
-
-rm tasmax_day_${model}_${exp}_${ens}_18500101-20051231.nc
+        rm ${var}_day_${model}_${exp}_${ens}_18500101-20051231.nc
+    done
+done
