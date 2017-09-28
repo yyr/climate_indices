cd94bd2659601060fbd2c77341bf99f3345d8b75
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-29 01:03:02 +0530

    * gfdlcm3.sh: similar to previous commits.

--- gfdlcm3.sh
+++ gfdlcm3.sh
@@ -1,118 +1,64 @@
 #!/bin/bash
 # Created: Wednesday, July 12 2017
-exp=${1:-historical}
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_FILE_DIR/fun.bash
+
+# exp=${1:-historical}
+# exmts=(historical)
+exmts=(historical historicalNat historicalGHG historicalMisc)
+
+vars=(pr)
+# vars=(pr tas tasmax tasmin)
+
 model=GFDL-CM3
+ens=r1i1p1
 
-case ${exp} in
-    historical )
-        dir=hist
-        ;;
-    historicalNat )
-        dir=histNat
-             ;;
-    historicalMisc )
-        dir=histMisc
-        ;;
-    historicalGHG )
-        dir=histGHG
-        ;;
-esac
+for exp in ${exmts[@]}; do
+    case ${exp} in
+        historical )
+            dir=hist
+            ;;
+        historicalNat )
+            dir=histNat
+            ;;
+        historicalMisc )
+            dir=histMisc
+            ;;
+        historicalGHG )
+            dir=histGHG
+            ;;
+    esac
 
-cd ${dir}
+    for var in "${vars[@]}"
+    do
+        cd $THIS_FILE_DIR/${dir}
 
-echo "Started processing ${exp} in ${dir} directory."
+        cdo -mergetime \
+            ${var}_day_${model}_${exp}_${ens}_19000101-19041231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19050101-19091231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19100101-19141231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19150101-19191231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19200101-19241231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19250101-19291231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19300101-19341231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19350101-19391231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19400101-19441231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19450101-19491231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19500101-19541231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19550101-19591231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19600101-19641231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19650101-19691231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19700101-19741231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19750101-19791231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19800101-19841231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19850101-19891231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19900101-19941231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19950101-19991231.nc \
+            ${var}_day_${model}_${exp}_${ens}_20000101-20041231.nc \
+            ${var}_day_${model}_${exp}_${ens}_20050101-20051231.nc \
+            ${var}_day_${model}_${exp}_${ens}_19000101-20051231.nc
 
-if ls -U *${model}*${exp}*r180x100* 1> /dev/null 2>&1; then
-    echo Files seems already processed.
-    exit
-fi
-
-cdo -mergetime \
-    tasmax_day_${model}_${exp}_r1i1p1_19000101-19041231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19050101-19091231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19100101-19141231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19150101-19191231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19200101-19241231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19250101-19291231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19300101-19341231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19350101-19391231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19400101-19441231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19450101-19491231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19500101-19541231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19550101-19591231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19600101-19641231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19650101-19691231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19700101-19741231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19750101-19791231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19800101-19841231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19850101-19891231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19900101-19941231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19950101-19991231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_20000101-20041231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_20050101-20051231.nc \
-    tasmax_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
-
-cdo -remapbil,r180x100 tasmax_day_${model}_${exp}_r1i1p1_19000101-20051231.nc\
-    tasmax_day_${model}_${exp}_r1i1p1_19000101-20051231_r180x100.nc
-
-rm tasmax_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
-
-cdo -mergetime \
-    tasmin_day_${model}_${exp}_r1i1p1_19000101-19041231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19050101-19091231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19100101-19141231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19150101-19191231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19200101-19241231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19250101-19291231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19300101-19341231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19350101-19391231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19400101-19441231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19450101-19491231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19500101-19541231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19550101-19591231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19600101-19641231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19650101-19691231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19700101-19741231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19750101-19791231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19800101-19841231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19850101-19891231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19900101-19941231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19950101-19991231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_20000101-20041231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_20050101-20051231.nc \
-    tasmin_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
-
-cdo -remapbil,r180x100 tasmin_day_${model}_${exp}_r1i1p1_19000101-20051231.nc\
-    tasmin_day_${model}_${exp}_r1i1p1_19000101-20051231_r180x100.nc
-
-rm tasmin_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
-
-cdo -mergetime \
-    tas_day_${model}_${exp}_r1i1p1_19000101-19041231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19050101-19091231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19100101-19141231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19150101-19191231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19200101-19241231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19250101-19291231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19300101-19341231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19350101-19391231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19400101-19441231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19450101-19491231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19500101-19541231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19550101-19591231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19600101-19641231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19650101-19691231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19700101-19741231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19750101-19791231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19800101-19841231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19850101-19891231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19900101-19941231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19950101-19991231.nc \
-    tas_day_${model}_${exp}_r1i1p1_20000101-20041231.nc \
-    tas_day_${model}_${exp}_r1i1p1_20050101-20051231.nc \
-    tas_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
-
-cdo -remapbil,r180x100 tas_day_${model}_${exp}_r1i1p1_19000101-20051231.nc\
-    tas_day_${model}_${exp}_r1i1p1_19000101-20051231_r180x100.nc
-
-rm tas_day_${model}_${exp}_r1i1p1_19000101-20051231.nc
+        cdo -remapbil,r180x100 ${var}_day_${model}_${exp}_${ens}_19000101-20051231.nc\
+            ${var}_day_${model}_${exp}_${ens}_19000101-20051231_r180x100.nc
+    done
+done
