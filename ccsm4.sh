654ac4a3368a3f80134e342510ca2bffd91ec959
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-28 19:03:00 +0530

    * ccsm4.sh: order change.

--- ccsm4.sh
+++ ccsm4.sh
@@ -6,7 +6,7 @@ source $THIS_FILE_DIR/fun.bash
 
 # exp=${1:-historical}
 # exmts=(historical)
-exmts=(historical historicalNat historicalMisc historicalGHG)
+exmts=(historical historicalNat historicalGHG historicalMisc)
 
 vars=(pr)
 # vars=(pr tas tasmax tasmin)
