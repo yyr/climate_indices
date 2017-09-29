7e7c901c3e15c6a2b2c7ed521dffea55f76bfafa
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-30 05:05:03 +0530

    * wd.sh: first implement.

--- fun.bash
+++ fun.bash
@@ -84,6 +84,32 @@ function ind_cn()
         ${model}_${exp}_CND_1900-2005.nc
 }
 
+function ind_wd()
+{
+    var=$1
+    exp=$2
+    model=$3
+
+    # Calculate indcices for each year.
+    for i in $(seq 1900 2005); do
+        cdo -eca_tn90p \
+            -selyear,$i \
+            ${var}_day_${model}_${exp}_r*_19000101-20051231_r180x100.nc \
+            ${var}_${model}_${exp}_ydpctl90p.nc \
+            ${model}_${exp}_WD_$i.nc
+    done
+
+    # merge time of indices and convert to days.
+    cdo mergetime \
+        ${model}_${exp}_WD_????.nc \
+        ${model}_${exp}_WD_1900-2005.nc
+
+    cdo -mulc,3.65 \
+        ${model}_${exp}_WD_1900-2005.nc \
+        ${model}_${exp}_WDD_1900-2005.nc
+}
+
+
 function ensmean()
 {
     ind="${1}"
@@ -101,5 +127,5 @@ function ensmean()
 
     cdo ensmean \
          ${str} \
-         ${exp}_CN_1900_2005_ensmean.nc
+         ${exp}_${ind}_1900_2005_ensmean.nc
 }
---
+++ wd.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Created: Thursday, July 27 2017
+set -x
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_FILE_DIR/fun.bash
+var=tasmax
+
+models=(NorESM1-M IPSL-CM5A-LR CCSM4 CanESM2 GFDL-CM3) # GFDL-ESM2M
+exmts=(historical historicalNat historicalGHG historicalMisc)
+
+for exp in ${exmts[@]};
+do
+    cd ${THIS_FILE_DIR}/${exp}
+    for model in "${models[@]}"
+    do
+
+        # calculate day 90th percentile
+        y90pctl ${var} ${exp} ${model}
+
+        # Calculate cold_nights indices merge time of indices
+        ind_wd ${var} ${exp} ${model}
+    done
+
+    # TODO: calculate ensemble mean
+    ensmean WD ${exp} ${models[@]}
+    ensmean WDD ${exp} ${models[@]}
+
+    # Calculate trend.
+    cdo -trend \
+        -selyear,1951/2005 \
+        ${exp}_WD_1900_2005_ensmean.nc \
+        ${exp}_WD_1951-2005_mean.nc \
+        ${exp}_WD_1951-2005_trend.nc
+    cdo -trend \
+        -selyear,1951/2005 \
+        ${exp}_WDD_1900_2005_ensmean.nc \
+        ${exp}_WDD_1951-2005_mean.nc \
+        ${exp}_WDD_1951-2005_trend.nc
+done
+
+#
+# # Mask Sea.
+# cdo -div ${model}_${exp}_WDD_1900-2005.nc ../landmask.nc ${model}_${exp}_WDD_1900-2005.land.nc
+# cdo -div ${model}_${exp}_WD_1900-2005.nc ../landmask.nc ${model}_${exp}_WD_1900-2005.land.nc
+
+# # field mean for global trend over the years.
+# cdo -fldmean ${model}_${exp}_WDD_1900-2005.land.nc ${model}_${exp}_WDD_1900-2005_glmean.nc
+# cdo -fldmean ${model}_${exp}_WD_1900-2005.land.nc ${model}_${exp}_WD_1900-2005_glmean.nc
+
+# # calculate anomolies.
+# cdo -sub ${model}_${exp}_WD_1900-2005_glmean.nc -timmean -selyear,1961/1990 ${model}_${exp}_WD_1900-2005_glmean.nc ${model}_${exp}_WD_1900-2005_glanom.nc
+# cdo -sub ${model}_${exp}_WDD_1900-2005_glmean.nc -timmean -selyear,1961/1990 ${model}_${exp}_WDD_1900-2005_glmean.nc ${model}_${exp}_WDD_1900-2005_glanom.nc
