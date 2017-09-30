e4b4f5dcbc9749331b7626dca0b815bd510a3d92
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-09-30 06:49:26 +0530

    * wsdi.sh: warm spell duration index.

---
+++ wsdi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Created: Thursday, July 27 2017
+set -x
+THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $THIS_FILE_DIR/fun.bash
+
+var=tas
+
+models=(NorESM1-M IPSL-CM5A-LR CCSM4 CanESM2 GFDL-CM3) # GFDL-ESM2M
+exmts=(historical historicalNat historicalGHG historicalMisc)
+
+for exp in ${exmts[@]};
+do
+    cd ${THIS_FILE_DIR}/${exp}
+    for model in "${models[@]}"
+    do
+
+        # calculate day 90th percentile
+        y90pctl ${var} ${exp} ${model}
+
+        # Calculate cold_nights indices merge time of indices
+        ind_wsdi ${var} ${exp} ${model}
+    done
+
+    # TODO: calculate ensemble mean
+    ensmean WSDI ${exp} ${models[@]}
+
+    # Calculate trend.
+    cdo -trend \
+        -selyear,1951/2005 \
+        ${exp}_WSDI_1900_2005_ensmean.nc \
+        ${exp}_WSDI_1951-2005_mean.nc \
+        ${exp}_WSDI_1951-2005_trend.nc
+done
